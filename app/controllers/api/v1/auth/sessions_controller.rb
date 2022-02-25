e51ac39d6c9de67ac1dd42338424e6985c3b11f5
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-02-25 15:26:43 +0900

    [Add] #10 ログインユーザーのもらったいいね総数API設定

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -5,7 +5,8 @@ class Api::V1::Auth::SessionsController < ApplicationController
             name: current_api_v1_user.name,
             email: current_api_v1_user.email,
             emotion: current_api_v1_user.emotion,
-            posts: current_api_v1_user.posts.map {|post| {id: post.id, content: post.content, emotion: post.emotion, likes: post.likes, created_at: post.created_at}}
+            posts: current_api_v1_user.posts.map {|post| {id: post.id, content: post.content, emotion: post.emotion, likes: post.likes, created_at: post.created_at}},
+            likes: current_api_v1_user.likes.where(post_id: Post.where(user_id: current_api_v1_user.id)).count + 1
         }
         if current_api_v1_user
             render json: {is_login: true, data: user_list }
